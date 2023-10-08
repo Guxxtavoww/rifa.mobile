31a02406f60d7badbf385b9c4d97b68c3d2cce2f
Gustavo <gustavoaugustocar@hotmail.com>
2023-10-08 19:22:20 -0300

    adding custom font

--- src/utils/fetch-fonts.util.ts
+++ src/utils/fetch-fonts.util.ts
@@ -1,9 +1,9 @@
-import * as Font from 'expo-font';
+import { loadAsync } from 'expo-font';
 
 import { THEME } from '@/styles/theme';
 
 export const fetchFonts = () =>
-  Font.loadAsync({
+  loadAsync({
     [THEME.fonts.regular]: require('../assets/fonts/Poppins-Regular.ttf'),
     [THEME.fonts.medium]: require('../assets/fonts/Poppins-Medium.ttf'),
     [THEME.fonts.bold]: require('../assets/fonts/Poppins-Bold.ttf'),
