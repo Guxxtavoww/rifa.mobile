c3dc907eb24b2bfb05f1a4862e8177e1d71d69bd
Gustavo <gustavoaugustocar@hotmail.com>
2023-10-09 11:11:00 -0300

    adding stuff

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
 
 import Routes from '@/routes';
 import { THEME } from '@/styles/theme';
 import { fetchFonts } from '@/utils/fetch-fonts.util';
-import { commonStyles } from '@/styles/common.styles';
 
 export default function App() {
   const [dataLoaded, setDataLoaded] = useState(false);
@@ -22,12 +20,12 @@ export default function App() {
   }
 
   return (
-    <SafeAreaView style={commonStyles.container}>
+    <>
       <StatusBar
         animated
         backgroundColor={THEME.colors.screen_white_background}
       />
       <Routes />
-    </SafeAreaView>
+    </>
   );
 }
