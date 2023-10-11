b2e609589b316e170024e6a78023e6ed4740c137
Gustavo <gustavoaugustocar@hotmail.com>
2023-10-11 13:58:22 -0300

    adding stuff

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 
 import StackRoutes from './stack.routes';
+import { useRedux } from '@/hooks';
 
 export default function Routes() {
+  const hasUserData = useRedux().useAppSelector(
+    (state) => !!state.auth.user_data && !!state.auth.access_token
+  );
+
   return (
     <NavigationContainer>
-      <StackRoutes />
+      {hasUserData ? null : <StackRoutes />}
     </NavigationContainer>
   );
 }
