300a74ce48b22a3b4c8098f683719b5d2e3138a3
Gustavo <gustavoaugustocar@hotmail.com>
2023-10-11 15:02:57 -0300

    adding stuff

--- src/contexts/CustomToastContext/index.tsx
+++ src/contexts/CustomToastContext/index.tsx
@@ -43,7 +43,7 @@ export const CustomToastProvider: FCWithChildren<{}, true> = ({ children }) => {
         fontFamily: THEME.fonts.medium,
       },
     });
-  }, []);
+  }, [nbToast]);
 
   return (
     <CustomToastContext.Provider value={{ toast }}>
