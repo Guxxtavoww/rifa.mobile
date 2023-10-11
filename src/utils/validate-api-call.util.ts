d9c15884b1771cc1d5ed13393d1a1f7188f1ac50
Gustavo <gustavoaugustocar@hotmail.com>
2023-10-11 16:22:00 -0300

    adding stuff

---
+++ src/utils/validate-api-call.util.ts
@@ -0,0 +1,53 @@
+import api from '../api/api';
+import { AxiosRequestConfig, Method } from 'axios';
+import { ZodSchema, ZodTypeDef, z } from 'zod';
+
+interface iValidatedAPICallProps<
+  TOutput = any,
+  TDef extends ZodTypeDef = ZodTypeDef,
+  TInput = TOutput
+> extends Omit<
+    AxiosRequestConfig<TOutput>,
+    'method' | 'url' | 'data' | 'params'
+  > {
+  body?: any;
+  method?: Method;
+  endpoint: string;
+  hideErrorMessage?: boolean;
+  customToastErrorMessage?: string;
+  zodSchema: ZodSchema<TOutput, TDef, TInput>;
+  params?: Record<string, Maybe<string | number | Date>>;
+}
+
+export async function validateApiCall<T>({
+  body,
+  endpoint,
+  params,
+  method = 'GET',
+  customToastErrorMessage,
+  zodSchema,
+  hideErrorMessage,
+  ...rest
+}: iValidatedAPICallProps<T>) {
+  try {
+    const response = await api.request({
+      method,
+      url: endpoint,
+      data: body,
+      params,
+      ...rest,
+    });
+
+    const responseParsedData = await zodSchema.parseAsync(response.data);
+
+    return Promise.resolve(responseParsedData);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      console.error(
+        `API não respondeu da maneira esperada cheque o ENDPOINT: ${endpoint} com o método ${method}`
+      );
+    }
+
+    return Promise.reject(error);
+  }
+}
