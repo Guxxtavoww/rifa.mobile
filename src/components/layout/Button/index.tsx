4601612e28fe8298c9bedbef5c64a93b1b9eb206
Gustavo <gustavoaugustocar@hotmail.com>
2023-10-13 11:42:06 -0300

    adding drawer navigation

--- src/components/layout/Button/index.tsx
+++ src/components/layout/Button/index.tsx
@@ -3,8 +3,7 @@ import { IButtonProps, Button as NBButton } from 'native-base';
 
 import { THEME } from '@/styles/theme.styles';
 
-import Text from '../Text';
-import { iTextProps } from '../Text';
+import Text, { iTextProps } from '../Text';
 
 export type ButtonProps = { buttonProps?: IButtonProps; textProps: iTextProps };
 
@@ -13,7 +12,6 @@ const Button: React.FC<ButtonProps> = ({ buttonProps, textProps }) => (
     w={buttonProps?.w ?? 'full'}
     h={buttonProps?.h ?? '12'}
     bg={buttonProps?.bg ?? THEME.colors.orange_color}
-    fontFamily={THEME.fonts.medium}
     {...buttonProps}
   >
     <Text {...textProps} />
