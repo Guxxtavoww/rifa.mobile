d9cafa359f947f41c28200bbf7eb6cbd380f199a
Gustavo <gustavoaugustocar@hotmail.com>
2023-10-19 09:17:32 -0300

    changing api url

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -5,7 +5,7 @@ export const envSchema = z.object({
     .string()
     .min(0)
     .optional()
-    .default('http://192.168.100.143:5000/server'),
+    .default('http://192.168.15.46:5000/server'),
   EXPO_FIREBASE_API_KEY: z
     .string()
     .optional()
@@ -17,7 +17,7 @@ export const envSchema = z.object({
   EXPO_ENCRYPTION_KEY: z
     .string()
     .optional()
-    .default(
+    .default( 
       'QT0datqlThEu51mc4VlV7iRWXVa1gAP85ZAu44VvvDlkpYVkFzSRRlHSOzsubDMN45OBQW6UA3RPg4TCvrTOmhQUeF5XPuSdcD0R2At6pdaLwAKnOtILg13Ha6ymIgjv8glodvem3hWLmpHIhNBiaXtf8wqpAoGADH5a8OhvKOtd8EChGXyp9LDWHRw9vbyNgi9dQXltgyoUBb1jDllgoJSRHgRFUvyvbbImR5c03JwqtiQ8siWTC9G5WGeSjcSNt9fVmG7W1L14MbrGJj8fFns7xrOlasnlPdgA5NCONtIsZY2DKZr0drhPhZBcWJlFxkCgYAn4SOPEo6hjKNhA6vER7fSxDEVsDgrDh3YgAWpvUdlaqBxqOyAqi600YugQZGHK2lv7vNYOdmrunuIx7BPuDqYbjtRR4Mc9bVQAZbXSLXMl7j2RWwKfNhLSJbk9LX4EoVtTgLjvOUE4tAdq9fFgpqdwLwzqPTO9kECP4CQKBgH6tOxcNxGuXUideluAn3H2KeyyznZMJ7oCvzf26XpTAMI243OoeftiKVMgxuZ7hjwqfnVHXABc4i5gchr9RzSb1hZIqFzq2YGmbppg5Ok2cgwalDoDBi21bRf8aDRweL62mO7aPnCQZ58j5W72PB8BAr6xg0Oro25O4os'
     ),
 });
