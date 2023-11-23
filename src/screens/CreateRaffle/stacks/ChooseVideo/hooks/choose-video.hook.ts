342026e72de00781072e6ef84afdf70160b41de2
Gustavo <gustavoaugustocar@hotmail.com>
2023-11-23 14:53:25 -0300

    Adding Choose video screen, polhishment needed

--- src/screens/CreateRaffle/stacks/ChooseVideo/hooks/choose-video.hook.ts
+++ src/screens/CreateRaffle/stacks/ChooseVideo/hooks/choose-video.hook.ts
@@ -8,33 +8,48 @@ export function useChooseVideo(replace: ScreenProps['navigation']['replace']) {
   const [videoUri, setVideoUri] = useState<string>();
 
   const handleImportVideo = useCallback(async () => {
-    await handlePermission();
+    try {
+      await handlePermission();
 
-    const videoResult = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
-      allowsMultipleSelection: false,
-      allowsEditing: true,
-      quality: 1,
-      selectionLimit: 1,
-      videoMaxDuration: 60,
-      videoQuality: ImagePicker.UIImagePickerControllerQualityType.Medium,
-      aspect: [4, 3],
-    });
-
-    if (videoResult.canceled) return;
-
-    const chosenVideo = videoResult.assets[0]!;
-    const maxSizeInBytes = 175 * 1024 * 1024;
-
-    if (chosenVideo.fileSize && chosenVideo.fileSize > maxSizeInBytes) {
-      toast('Tamanho máximo de 175Mb execido!', {
-        status: 'warning',
+      const videoResult = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+        allowsMultipleSelection: false,
+        allowsEditing: true,
+        quality: 1,
+        selectionLimit: 1,
+        videoMaxDuration: 60,
+        videoQuality: ImagePicker.UIImagePickerControllerQualityType.Medium,
+        aspect: [4, 3],
       });
 
-      return;
-    }
+      if (videoResult.canceled) return;
 
-    setVideoUri(chosenVideo.uri);
+      const chosenVideo = videoResult.assets[0]!;
+      const maxSizeInBytes = 175 * 1024 * 1024;
+
+      if (!chosenVideo.fileSize || !chosenVideo.duration)
+        throw 'Erro! tente novamente';
+
+      if (chosenVideo.fileSize > maxSizeInBytes || chosenVideo.duration > 60) {
+        const message =
+          chosenVideo.fileSize > maxSizeInBytes
+            ? 'Tamanho máximo de 175Mb excedido!'
+            : 'Tamanho máximo do vídeo excedido';
+
+        toast(message, {
+          status: 'warning',
+        });
+
+        return;
+      }
+
+      setVideoUri(chosenVideo.uri);
+    } catch (error) {
+      console.error('Error during video import:', error);
+      toast('An error occurred while importing the video', {
+        status: 'error',
+      });
+    }
   }, []);
 
   const handleNextButtonPress = useCallback(() => {
